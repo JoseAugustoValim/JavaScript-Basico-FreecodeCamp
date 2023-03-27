d43c6abe312d1d12af2b47c61862ab9d4fdd6349
José A. Valim <joseaugustovalim1994@gmail.com>
2023-03-27 11:21:48 -0300

    Add exercício dejssobredejssobre objetos

---
+++ 1 - Criar objetos/exercicios.js
@@ -0,0 +1,43 @@
+ //Naruto: Faça um objeto que represente um personagem chamado myNarutoCharacter que contém as propriedades name (uma string), age (um número), village (uma string) e jutsus (um array).
+const myNarutoCharacter = {
+  name: 'Naruto',
+  age: 9,
+  village:'Konoha',
+  jutsus:['Jutsu Clone das Sombras', 'Rasengan']
+}
+ //Bleach: Faça um objeto que represente um personagem chamado myBleachCharacter que contém as propriedades name (uma string), division (uma string), zanpakutou (uma string) e hollow (um objeto com propriedades name, powerLevel e mask).
+const myBleachCharacter = {
+  name: 'Ichigo',
+  division: 'Sem divisao',
+  zampakutou: 'Zangetsu',
+  hollow = {
+    name = 'Ichigo Branco',
+    powerLevel = '10',
+    mask = 'Caveira com marcas vermelhas'
+  }
+
+}
+ //One Piece: Faça um objeto que represente um personagem chamado myOnePieceCharacter que contém as propriedades name (uma string), bounty (um número), devilFruit (um objeto com propriedades name e type) e crew (um array com os nomes dos tripulantes).
+ const myOnePieceCharacter = {
+  name : 'Monkey D. Luffy',
+  bounty: 3000000000,
+  devilFruit = {
+    name: 'Gomu Gomu',
+    type : 'borracha',
+  },
+  crew : ['Luffy', 'Zoro', 'Nami','Usopp','Robin', 'Chopper', 'Robin', 'Franky', 'Broki', 'Jinbei'],
+ }
+
+ //Naruto: Faça um objeto que represente uma missão chamada myNarutoMission que contém as propriedades name (uma string), rank (uma string), difficulty (um número) e rewards (um objeto com propriedades money e items).
+
+ //Bleach: Faça um objeto que represente uma batalha chamada myBleachBattle que contém as propriedades location (uma string), winner (uma string), participants (um array com os nomes dos personagens) e casualties (um objeto com propriedades killed e injured).
+
+ //One Piece: Faça um objeto que represente um tesouro chamado myOnePieceTreasure que contém as propriedades name (uma string), value (um número), location (uma string) e guardians (um array com os nomes dos personagens que o protegem).
+
+ //Naruto: Faça um objeto que represente um clã chamado myNarutoClan que contém as propriedades name (uma string), members (um array com os nomes dos membros), village (uma string) e techniques (um objeto com propriedades offensive e defensive).
+
+ //Bleach: Faça um objeto que represente uma sociedade secreta chamada myBleachSecretSociety que contém as propriedades name (uma string), members (um array com os nomes dos membros), leader (uma string) e goals (um objeto com propriedades shortTerm e longTerm).
+
+ //One Piece: Faça um objeto que represente um navio pirata chamado myOnePieceShip que contém as propriedades name (uma string), crew (um array com os nomes dos tripulantes), captain (uma string) e abilities (um objeto com propriedades speed e firepower).
+
+ //Naruto: Faça um objeto que represente um item de inventário chamado myNarutoItem que contém as propriedades name (uma string), quantity (um número), type (uma string) e effects (um objeto com propriedades healing e damage).
